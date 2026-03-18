fcb0bfb5a041a9c67afe886bdac72cdad50b3401
kormonditamas2222 <t.kormond2006@gmail.com>
2026-03-18 08:22:49 +0100

    Add project files.

---
+++ Keresztrejtveny/KeresztrejtvenyRacs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keresztrejtveny
+{
+    internal class KeresztrejtvenyRacs
+    {
+        private List<string> adatsorok;
+        private char[,] racs;
+        private int[,] sorszamok;
+
+        public KeresztrejtvenyRacs(string forras)
+        {
+
+        }
+        public int OszlopokDb => adatsorok[0].Length;
+        public int SorokDb => adatsorok.Count;
+        private void BeolvasAdatsorok(string forras)
+        {
+
+        }
+    }
+}
