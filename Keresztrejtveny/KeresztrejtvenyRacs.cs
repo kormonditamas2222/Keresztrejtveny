848dc9269319b5d05c3f57458ea779a42affd8dd
kormonditamas2222 <t.kormond2006@gmail.com>
2026-03-18 08:29:27 +0100

    3

--- Keresztrejtveny/KeresztrejtvenyRacs.cs
+++ Keresztrejtveny/KeresztrejtvenyRacs.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Keresztrejtveny
 {
@@ -14,13 +15,29 @@ namespace Keresztrejtveny
 
         public KeresztrejtvenyRacs(string forras)
         {
-
+            BeolvasAdatsorok(forras);
+            racs = new char[SorokDb, OszlopokDb];
+            sorszamok = new int[SorokDb, OszlopokDb];
+            FeltoltRacs()
         }
         public int OszlopokDb => adatsorok[0].Length;
         public int SorokDb => adatsorok.Count;
         private void BeolvasAdatsorok(string forras)
         {
-
+            foreach (var sor in File.ReadAllLines(forras))
+            {
+                adatsorok.Add(sor);
+            }
+        }
+        private void FeltoltRacs()
+        {
+            for (int i = 0; i < SorokDb; i++)
+            {
+                for (int j = 0; i < OszlopokDb; j++)
+                {
+                    racs[i, j] = adatsorok[i][j];
+                }
+            }
         }
     }
 }
